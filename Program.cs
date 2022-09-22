87aeb3ff06782781d7f14226df3c4b1476cb764c
niqee <ddesantana7@gmail.com>
2022-09-21 21:55:29 -0300

    adicionando exemplo array inicial

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+int[] arrayInteiros = new int[3];
+
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+//arrayInteiros[3] = 1;
+
+for (int contador =0;contador < arrayInteiros.Length;contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
+}
