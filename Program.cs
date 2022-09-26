c1c04dff3af4472d63396d96e8c7d85ab28eab07
Dominique Santana <105083414+niqee@users.noreply.github.com>
2022-09-26 17:39:48 -0300

    Update Program.cs
    Adicionando e explicando o uso dos métodos  Resize e Copy.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 
 
+
+
+
 int[] arrayInteiros = new int[3];
 
 arrayInteiros[0] = 72;
@@ -20,7 +23,7 @@ for (int contador =0;contador < arrayInteiros.Length;contador++)
 //forma alternativa de percorrer uma lista sem um contador
 Console.WriteLine("Percorrendo o array com o FOREACH");
 
-foreach (int valor in arrayInteiros) // a variável "valor" recebe os números que estão em arrayInteiros em cada interação
+foreach (int valor in arrayInteiros) // a var "valor" recebe os números que estão em arrayInteiros em cada interação
 {
     Console.WriteLine(valor);  
 }
@@ -35,3 +38,23 @@ foreach (int valor2 in arrayInteiros)
     Console.WriteLine($"Posição N° {contadorForeach} - {valor2}"); 
     contadorForeach++;
 }
+
+
+//Redimensionando um Array
+
+Array.Resize(ref arrayInteiros, arrayInteiros.Length *2);
+
+/*Após isso o array em questão passa a ter o dobro do tamanho original
+É possível também reduzir o tamanho do mesmo usando o mesmo código
+"Um array nasce e morre com uma capacidade fixa", então o que o resize faz é criar uma cópia
+do array antigo porém com uma capacidade maior, ele também copia todos os elementos do array antigo para o novo. 
+*/
+
+//Copiar um Array para outro
+
+int[] arrayInteirosDobrado =  new int[arrayInteiros.Length * 2];
+Array.Copy(arrayInteiros,arrayInteirosDobrado,arrayInteiros.Length);
+
+/* Faz praticamente o que o resize faz, porém o copy transfere os elementos para uma outra
+variável na qual foi declarada antes.
+*/
