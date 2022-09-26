30dc674c40478bd25417ae3326b0dfb347e3b8a0
Dominique Santana <105083414+niqee@users.noreply.github.com>
2022-09-26 12:39:50 -0300

    Update Program.cs
    Adicionando mais dois exemplos de como percorrer uma array

--- Program.cs
+++ Program.cs
@@ -1,4 +1,3 @@
-﻿
 
 
 int[] arrayInteiros = new int[3];
@@ -8,7 +7,31 @@ arrayInteiros[1] = 64;
 arrayInteiros[2] = 50;
 //arrayInteiros[3] = 1;
 
+
+Console.WriteLine("Percorrendo o array com o FOR");
+
 for (int contador =0;contador < arrayInteiros.Length;contador++)
 {
     Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
+}
+
+
+
+//forma alternativa de percorrer uma lista sem um contador
+Console.WriteLine("Percorrendo o array com o FOREACH");
+
+foreach (int valor in arrayInteiros) // a variável "valor" recebe os números que estão em arrayInteiros em cada interação
+{
+    Console.WriteLine(valor);  
+}
+
+//ter o mesmo retorno ao exemplo 1, porém usando foreach com uma var contador
+
+
+Console.WriteLine("Percorrendo o array com o FOREACH");
+int contadorForeach = 0;
+foreach (int valor2 in arrayInteiros) 
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {valor2}"); 
+    contadorForeach++;
 }
